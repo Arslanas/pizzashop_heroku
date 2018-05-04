c7a0384b822c58c4ea9c253ba78e58b9949e3438
Arslan <yamamoto1659@gmail.com>
2018-05-04 08:11:24 +0300

    After_data_filling_2_heroku

--- src/main/java/pizzaShop/temp/RegexTest.java
+++ src/main/java/pizzaShop/temp/RegexTest.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class RegexTest {
     public static void main(String[] args) {
-        String text = "+7 (960) 132-60-68";
+        String text = "+7 (960) 112-64-61";
         Pattern p = Pattern.compile("^\\+7 \\([0-9]{3}\\) [0-9]{3}-[0-9]{2}-[0-9]{2}$");
         Matcher matcher = p.matcher(text);
         System.out.println(matcher.matches());
